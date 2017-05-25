2d42f8636f1656988ccfb514ad2f1236f137c234
Christian Schleidt <cschleidt@hotmail.com>
2017-05-25 23:29:51 +0200

    Initial check-in of module boligbank

---
+++ boligbank/Views/LoginPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace boligbank
+{
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
